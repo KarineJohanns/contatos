42e16e1af988b38ba3fe457ac3c821b480f738a8
KarineJohanns <karinejohannsads@gmail.com>
2023-05-03 16:41:13 -0300

    Implementado componentes view

---
+++ resources/views/partials/pesquisa.blade.php
@@ -0,0 +1,22 @@
+ <!-- menu lateral -->
+ <div id="menu" class="container">
+     <ul>
+         <li><a href="#"><img src="{{ asset('img/contato-caderno.svg')}}">Contatos</a></li>
+         <!-- <li><a href="#"><img src="{{ asset('img/agenda.svg')}}">Agenda</a></li> -->
+     </ul>
+ </div>
+
+ <!-- busca e adicionar contato -->
+ <div class="container">
+     <form>
+         <div class="search-container">
+             <div class="search-box flex-grow-1">
+                 <button type="submit"><i class="fa fa-search"></i></button>
+                 <input type="text" placeholder="Pesquisar...">
+             </div>
+             <button type="button" class="add-contact-btn"><i class="fa-solid fa-plus"></i> Adicionar contato</button>
+         </div>
+     </form>
+
+
+ </div>
