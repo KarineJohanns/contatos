11d721bf6ba43f363bd2a6da4125859f6580c222
KarineJohanns <karinejohannsads@gmail.com>
2023-05-05 16:05:59 -0300

    Criação da tela de login

--- resources/views/Login/index.blade.php
+++ resources/views/Login/index.blade.php
@@ -1,41 +1,57 @@
     <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Login</title>
 
         @include('partials.header')
 
     </head>
+
     <body class="antialiased">
-        <h1>Page login</h1>
         @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
         @endif
 
         @if(session('danger'))
-            <div class="alert alert-danger">
-                {{session('danger')}}
-            </div>
+        <div class="alert alert-danger">
+            {{session('danger')}}
+        </div>
         @endif
 
-        <form method="post" action="{{route('auth.user')}}">
-            @csrf
-            <label for="">Email:</label>
-            <input type="text" name="email">
-
-            <label for="">Senha:</label>
-            <input type="password" name="password">
-            <button type="submit">Logar</button>
-        </form>
-        <a href="{{route('registry')}}">Registre-se</a>
+        <div class="container-login">
+            <section class="login-container">
+                <article class="title">
+                    <h4 class="title-text">Lista de Contatos</h4>
+                    <p class="description">Faça login para acessar</p>
+                </article>
+                <form method="post" action="{{route('auth.user')}}" class="form-login">
+                    @csrf
+                    <label for="">Email*</label>
+                    <input type="text" name="email" placeholder="Seuemail@mail.com">
+                    <label for="">Senha*</label>
+                    <input type="password" name="password" placeholder="Senha">
+                    <a href="#" class="forgot-password">Esqueceu a senha?</a>
+                    <button type="submit">Entrar</button>
+                </form>
+                <article class="login-google">
+                    <div id="buttonDiv"></div>
+                </article>
+                <article class="signin">
+                    <p class="text">Não possui conta?<span><a href="{{route('registry')}}" class="signin-text"> Cadastre-se</a></span></p>
+                </article>
+            </section>
+        </div>
+        @include('partials.scripts')
     </body>
-</html>
+
+    </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,17 @@
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
 
 <!-- Styles -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
 <link href="{{ asset('css/pesquisa.css') }}" rel="stylesheet">
+<link href="{{ asset('css/login.css') }}" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
