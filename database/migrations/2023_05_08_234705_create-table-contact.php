57741b5c39bb266fc75997a5cac2803677782994
rafaelfernandes98 <rafael130140@gmail.com>
2023-05-08 21:48:04 -0300

    edit migration

--- database/migrations/2023_05_08_234705_create-table-contact.php
+++ database/migrations/2023_05_08_234705_create-table-contact.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->integer('number');
             $table->string('city');
             $table->string('note');
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->default(0);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->timestamps();
