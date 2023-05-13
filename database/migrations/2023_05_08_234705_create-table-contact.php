386300eef623a5421bb25651bcf03795fdc9eaa4
rafaelfernandes98 <rafael130140@gmail.com>
2023-05-12 22:52:33 -0300

    added nullable at coluns in the contact

--- database/migrations/2023_05_08_234705_create-table-contact.php
+++ database/migrations/2023_05_08_234705_create-table-contact.php
@@ -15,16 +15,16 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone');
-            $table->string('email');
-            $table->string('street');
-            $table->string('state');
-            $table->string('cpf');
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('street')->nullable();
+            $table->string('state')->nullable();
+            $table->string('cpf')->nullable();
             $table->string('phone-number');
-            $table->string('cep');
-            $table->integer('number');
-            $table->string('city');
-            $table->string('note');
+            $table->string('cep')->nullable();
+            $table->integer('number')->nullable();
+            $table->string('city')->nullable();
+            $table->string('note')->nullable();
             $table->unsignedBigInteger('user_id')->default(0);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
